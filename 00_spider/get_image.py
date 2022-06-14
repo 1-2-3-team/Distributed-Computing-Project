50ec494f29c6e2742679a84a17dd4a38f338b596
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-14 17:04:38 -0500

    Update Web Crawler

--- 00_spider/get_image.py
+++ 00_spider/get_image.py
@@ -1,3 +1,4 @@
-mport subprocess
+import subprocess
+import os
 p = subprocess.Popen(["scp", "/data/team5/chart.png", "lbgalvan@132.247.186.67:/home/lbgalvan/public_html/static/images/"])
 sts = os.waitpid(p.pid, 0)
