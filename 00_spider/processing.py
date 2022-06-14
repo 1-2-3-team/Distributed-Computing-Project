1c5e020f4cdc88682ed02cb33b7c59aabc72d6e0
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-14 01:02:59 -0500

    Update Web Crawler

--- 00_spider/processing.py
+++ 00_spider/processing.py
@@ -2,6 +2,7 @@
 import psycopg2
 from datetime import datetime
 import matplotlib as plt
+import numpy as np
 
 # Establish a connection to the database by creating a cursor object
 # The PostgreSQL server must be accessed through the PostgreSQL APP or Terminal Shell
@@ -17,7 +18,8 @@ cur = conn.cursor()
 cur.execute("""SELECT * FROM tweets""")
 query_results = cur.fetchall()
 new_query = query_results = query_results[-1]
-#print(new_query)
+
+
 positive = int(new_query[0])
 wpositive = int(new_query[1])
 spositive = int(new_query[2])
@@ -25,37 +27,18 @@ negative = int(new_query[3])
 wnegative = int(new_query[4])
 snegative = int(new_query[5])
 neutral = int(new_query[6])
+searchTerm = str(new_query[8])
 
-new_query = new_query.pop(7)
-new_query = new_query.pop(8)
+y = np.array([positive, wpositive, spositive,negative, wnegative, snegative, neutral])
+labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]', 'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
+sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
+colors = ['yellowgreen','lightgreen','darkgreen', 'blue', 'red','lightsalmon','darkred']
+patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+plt.legend(patches, labels, loc="best")
+plt.title('How people are reacting on ' + searchTerm + ' by analyzing Tweets.')
+plt.axis('equal')
+plt.tight_layout()
+plt.savefig('chart.png')
+
 
 conn.close()
-'''
-def plotPieChart(self, positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfSearchTerms):
-    labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
-              'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
-    sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
-    colors = ['yellowgreen','lightgreen','darkgreen', 'blue', 'red','lightsalmon','darkred']
-    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
-    plt.legend(patches, labels, loc="best")
-    plt.title('How people are reacting on ' + searchTerm + ' by analyzing ' + str(noOfSearchTerms) + ' Tweets.')
-    plt.axis('equal')
-    plt.tight_layout()
-    plt.savefig('chart.png')'''
-    
-
-#def percentage(self, part, whole):
-#    temp = 100 * float(part) / float(whole)
-#    return format(temp, '.2f')
-#
-#def plotPieChart(self, positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfSearchTerms):
-#    labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
-#            'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
-#    sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
-#    colors = ['yellowgreen','lightgreen','darkgreen', 'blue', 'red','lightsalmon','darkred']
-#    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
-#    plt.legend(patches, labels, loc="best")
-#    plt.title('How people are reacting on ' + searchTerm + ' by analyzing ' + str(noOfSearchTerms) + ' Tweets.')
-#    plt.axis('equal')
-#    plt.tight_layout()
-#    plt.savefig('chart.png')
