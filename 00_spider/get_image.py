12c350012611212575f9a30c6f8be93fe01eabd2
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-16 08:58:29 -0500

    updated site

--- 00_spider/get_image.py
+++ 00_spider/get_image.py
@@ -24,7 +24,7 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 import subprocess
 import os
 
-p = subprocess.Popen(["scp", "/data/team5/chart.png", "lbgalvan@132.247.186.67:/home/lbgalvan/public_html/img/"])
+p = subprocess.Popen(["scp", "/data/team5/chart.png", "lbgalvan@132.247.186.67:/home/lbgalvan/public_html/static/images/"])
 html =  subprocess.Popen(["scp", "-r", "/home/lbgalvan/git/Distributed-Computing-Project/public_html/", "lbgalvan@132.247.186.67:/home/lbgalvan/"])
 r = subprocess.Popen(["rm", "/home/lbgalvan/result.csv"])
 sts = os.waitpid(p.pid, 0)
