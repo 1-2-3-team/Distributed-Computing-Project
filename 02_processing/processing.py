41e1e867cb7925d63c5ba60206bd6e59bda702a5
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-15 21:45:02 -0500

    updated site

--- 02_processing/processing.py
+++ 02_processing/processing.py
@@ -34,10 +34,10 @@ time = str(new_query[9])
 y = np.array([positive, wpositive, spositive,negative, wnegative, snegative, neutral])
 labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]', 'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
 sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
-colors = ['yellowgreen','lightgreen','darkgreen', 'blue', 'red','lightsalmon','darkred']
+colors = ['yellowgreen','lightgreen','darkgreen', 'grey', 'red','lightsalmon','darkred']
 patches, texts = plt.pie(sizes, colors=colors, startangle=90)
 plt.legend(patches, labels, loc="best")
-plt.title('How people are reacting on ' + searchTerm + ' by analyzing Tweets on ' + date + ' at ' + time)
+plt.title('How people are reacting on ' + searchTerm + ' on ' + date + ' at ' + time)
 plt.axis('equal')
 plt.tight_layout()
 plt.savefig('/data/team5/chart.png')
