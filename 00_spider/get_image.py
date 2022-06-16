b3d59958818809d2a0cd015baeef428682fb4d8d
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-16 08:44:03 -0500

    updated site

--- 00_spider/get_image.py
+++ 00_spider/get_image.py
@@ -21,10 +21,9 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-import csv
 import subprocess
 import os
-from turtle import home
+
 p = subprocess.Popen(["scp", "/data/team5/chart.png", "lbgalvan@132.247.186.67:/home/lbgalvan/public_html/img/"])
 html =  subprocess.Popen(["scp", "-r", "/home/lbgalvan/git/Distributed-Computing-Project/public_html/", "lbgalvan@132.247.186.67:/home/lbgalvan/"])
 r = subprocess.Popen(["rm", "/home/lbgalvan/result.csv"])
