0c24f80c2e8ad53848f9719b3b18d9eb01da8b25
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-17 18:59:23 -0500

    updated site

--- 02_processing/processing.py
+++ 02_processing/processing.py
@@ -84,7 +84,6 @@ plt.legend(
 )
 tant = ax.annotate("Results are given by taking a sample of 1000 tweets each 30 minutes a day.", xy=(0,0), xytext=(2,2),textcoords="offset points", bbox=dict(boxstyle="round", fc="w"), zorder=10)
 ax.add_artist(tant)
-size=9, horizontalalignment='right')
 ax.set_title('Tweets analized today ' + date + ' at ' + time)
 ax.figure.savefig('/data/team5/chart.png')
 
