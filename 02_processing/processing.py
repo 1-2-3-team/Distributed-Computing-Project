0fe2b7cffe9c433a642ba8279b103d3bde0c7449
GalvanLuis <galvan_luis@comunidad.unam.mx>
2022-06-17 18:25:09 -0500

    updated site

--- 02_processing/processing.py
+++ 02_processing/processing.py
@@ -42,6 +42,11 @@ cur.execute("""SELECT * FROM tweets""")
 query_results = cur.fetchall()
 new_query = query_results = query_results[-1]
 
+def autopct_generator(limit):
+    """Remove percent on small slices."""
+    def inner_autopct(pct):
+        return ('%.2f%%' % pct) if pct > limit else ''
+    return inner_autopct
 
 positive = int(new_query[0])
 wpositive = int(new_query[1])
@@ -57,14 +62,28 @@ time = str(new_query[9])
 y = np.array([positive, wpositive, spositive,negative, wnegative, snegative, neutral])
 labels = ['Positive', 'Weakly Positive','Strongly Positive', 'Neutral', 'Negative', 'Weakly Negative', 'Strongly Negative']
 sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
-explode = (0,0,0,0.1,0,0,0)
+NUM_COLORS = len(sizes)
 fig, ax = plt.subplots()
-#colors = ['yellowgreen','lightgreen','darkgreen', 'grey', 'red','lightsalmon','darkred']
-ax.pie(sizes, autopct='%.1f%%', shadow=True, startangle=90)
-ax.legend(labels, loc='best')
+theme = plt.get_cmap('bwr')
+ax.set_color_cycle([theme(
+    1. * i / NUM_COLORS) for i in range(NUM_COLORS)])
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 1.3, box.height])
+_, _, autotexts = ax.pie(
+    sizes, autopct=autopct_generator(7), startangle=90, radius=1.8 * 1000)
+for autotext in autotexts:
+    autotext.set_weight('bold')
+ax.axis('equal')
+total = sum(sizes)
+plt.legend(
+    loc='upper left',
+    labels=['%s, %1.1f%%' % (
+        l, (float(s) / total) * 100) for l, s in zip(labels, sizes)],
+    prop={'size': 12},
+    bbox_to_anchor=(0.0, 1),
+    bbox_transform=fig.transFigure
+)
 ax.set_title('Tweets analized')
-ax.set_aspect('equal')
-#plt.tight_layout()
 ax.figure.savefig('/data/team5/chart.png')
 
 
